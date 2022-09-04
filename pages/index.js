0c68393d9c185fd823938650f76c3cff890234db
Mateus <mateus@aprovatotal.com.br>
2022-09-04 01:28:56 -0300

    Pre commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react';
 import Page from '../components/Page';
-import GetGame from '../lib/gamesConfig';
+import GetGame, { GetAvailableGames } from '../lib/gamesConfig';
 import CreateGame from '../lib/shuffle';
 
 export default function Home() {
   const game = new GetGame('lotofacil');
 
   const generateNumbers = () => {
-    console.log(CreateGame(game.numbers).slice(0, game.minNumbers));
+    document.getElementById('numeros').innerHTML = CreateGame(game.numbers).slice(0, game.minNumbers)
   }
 
   return (
     <Page>
-      <h1>This is the Home page</h1>
 
       <button onClick={generateNumbers}>GERAR</button>
+
+      <div id="numeros"></div>
 
     </Page>
   );
